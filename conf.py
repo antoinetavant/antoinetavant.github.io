d1a001d6a05248f51648dd66adbdd8528f93377f
antoinetavant <antoinetavant@hotmail.fr>
2024-05-21 14:39:09 +0200

    reformat lists

--- conf.py
+++ conf.py
@@ -287,8 +287,8 @@ html_theme = 'pydata_sphinx_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    # 'github_button': False,
     "navigation_with_keys": False,
+    "github_url": "https://github.com/antoinetavant",
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
